1d7cb7541c0d3170aa0243a23e6c0efda3e090f5
TaiKonne <2124802010067@student.tdmu.edu.vn>
2023-10-31 11:09:15 +0700

    calender add get and show data

--- JoinProject/client/src/pages/Lich.js
+++ JoinProject/client/src/pages/Lich.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Scheduler } from "@aldabil/react-scheduler";
-import axios, { Axios } from "axios";
+import axios from "axios";
 import moment from "moment";
-import { Button } from 'primereact/button';
+
 
 let evenmain = [];
 
@@ -36,60 +36,6 @@ function Lich() {
       });
 
   }, []);
-
-  const [evenCourse, setEvenCourse] = useState([]);
-  const [sett, settt] = useState([]);
-
-
-
-  const even = [ // data mẫu
-
-    {
-      _id: "653fd4de9f7a2668438072c6",
-      id: 1,
-      title: "Lập trình web",
-      start: moment("2023/10/30 07:00"),
-      end: moment("2023/10/30 11:30"),
-    },
-    {
-      _id: "653fd5609f7a2668438072c7",
-      id: 2,
-      title: "Đồ án cơ sở ngành",
-      start: moment("2023/10/31 12:30"),
-      end: moment("2023/10/31 16:30"),
-    },
-  ];
-  // console.log(moment("2023/10/31 16:30"));
-  // var ime = moment("2023-10-30T04:00:00.000Z").format("YYYY/MM/DD HH:MM");
-  // console.log(moment("2023-10-30T04:00:00.000Z").format("YYYY/MM/DD HH:MM"));
-  // console.log(moment(ime));
-
-  // const AddTime = () => {
-  //   axios
-  //     .post("http://localhost:3002/lich", {
-  //       id: 1,
-  //       title: "Lập trình web",
-  //       start: moment("2023/10/30 07:00"),
-  //       end: moment("2023/10/30 11:30"),
-  //     })
-  //     .then(function (res) {
-  //       fetchDataPosts()
-  //     })
-  //     .catch(function (er) {
-  //       console.log(er);
-  //     })
-  // }
-
-  // const fetchDataPosts = () => {
-  //   axios
-  //     .get("http://localhost:3002/lich", {})
-  //     .then(function (response) {
-  //       // settt(response.data.dataPosts);
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error);
-  //     });
-  // }
 
   return (
 
