b6c81ec2019848f1b7270d758623540a688e8679
TaiKonne <2124802010067@student.tdmu.edu.vn>
2023-10-31 11:05:32 +0700

    calendar add method get data

--- JoinProject/client/src/pages/Lich.js
+++ JoinProject/client/src/pages/Lich.js
@@ -1,23 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { Scheduler } from "@aldabil/react-scheduler";
 import axios, { Axios } from "axios";
+import moment from "moment";
+import { Button } from 'primereact/button';
+
+let evenmain = [];
+
 function Lich() {
 
-  const [evenCourse, setEvenCourse] = useState([]);
-  const [evenn, setEvenn] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:3002/lich", {})
       .then(function (response) {
-        setEvenCourse(response.data.dataPosts);
+        let temp = response.data.dataPosts;
+        // setEvenCourse(response.data.dataPosts);
+        // console.log(response.data.dataPosts);
+        // console.log(evenCourse);
+        evenmain=[];
+        temp.map(item => {
+          var endTime = moment(item.end).format("YYYY/MM/DD HH:MM");
+          var startTime = moment(item.start).format("YYYY/MM/DD HH:MM");
+
+          const obj = {
+            id: item.id,
+            end: moment(endTime),
+            start: moment(startTime),
+            title: item.title,
+          }
+          evenmain.push(obj);
+        })
+        console.log(temp);
       })
       .catch(function (ex) {
         console.log(ex);
-      })
-    console.log("1", evenCourse);
-
+      });
 
   }, []);
+
+  const [evenCourse, setEvenCourse] = useState([]);
+  const [sett, settt] = useState([]);
+
+
 
   const even = [ // data mẫu
 
@@ -25,22 +48,53 @@ function Lich() {
       _id: "653fd4de9f7a2668438072c6",
       id: 1,
       title: "Lập trình web",
-      start: new Date("2023/10/30 07:00"),
-      end: new Date("2023/10/30 11:30"),
+      start: moment("2023/10/30 07:00"),
+      end: moment("2023/10/30 11:30"),
     },
     {
       _id: "653fd5609f7a2668438072c7",
       id: 2,
       title: "Đồ án cơ sở ngành",
-      start: new Date("2023/10/31 12:30"),
-      end: new Date("2023/10/31 16:30"),
+      start: moment("2023/10/31 12:30"),
+      end: moment("2023/10/31 16:30"),
     },
   ];
+  // console.log(moment("2023/10/31 16:30"));
+  // var ime = moment("2023-10-30T04:00:00.000Z").format("YYYY/MM/DD HH:MM");
+  // console.log(moment("2023-10-30T04:00:00.000Z").format("YYYY/MM/DD HH:MM"));
+  // console.log(moment(ime));
 
-  // console.log("1", even);
+  // const AddTime = () => {
+  //   axios
+  //     .post("http://localhost:3002/lich", {
+  //       id: 1,
+  //       title: "Lập trình web",
+  //       start: moment("2023/10/30 07:00"),
+  //       end: moment("2023/10/30 11:30"),
+  //     })
+  //     .then(function (res) {
+  //       fetchDataPosts()
+  //     })
+  //     .catch(function (er) {
+  //       console.log(er);
+  //     })
+  // }
+
+  // const fetchDataPosts = () => {
+  //   axios
+  //     .get("http://localhost:3002/lich", {})
+  //     .then(function (response) {
+  //       // settt(response.data.dataPosts);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }
+
   return (
 
     <div className="mt-5">
+      {/* <Button className="bg-black ml-20px" onClick={AddTime()} /> */}
       <Scheduler
         week={{
           weekDays: [0, 1, 2, 3, 4, 5, 6],
@@ -49,7 +103,7 @@ function Lich() {
           endHour: 18,
         }}
         view="week"
-        events={even}
+        events={evenmain}
       />
     </div>
   );
--- JoinProject/models/calendarModel.js
+++ JoinProject/models/calendarModel.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose");
 
 
-const CalenderSchema = new mongoose.Schema({
+const CalendarSchema = new mongoose.Schema({
     id: { type: String, default: "" },
     title: { type: String, default: "" },
-    start: { type: String },
-    end: { type: String },
+    start: { type: Date },
+    end: { type: Date },
 });
-
-module.exports = mongoose.model("calender", CalenderSchema);
+// {
+//     "_id": {
+//       "$oid": "653fd4de9f7a2668438072c6"
+//     },
+//     "end": {
+//       "$date": "2023-10-31T05:30:00.000Z"
+//     },
+//     "id": "1",
+//     "start": {
+//       "$date": "2023-10-30T00:00:00.000Z"
+//     },
+//     "title": "Đồ án cơ sở ngành"
+//   }
+module.exports = mongoose.model("calendar", CalendarSchema);
